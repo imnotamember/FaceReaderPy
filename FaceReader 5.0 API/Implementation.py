84da5986ac1456343a26724e8a99490d2d4663c5
Joshua Zosky <joshua.e.zosky@gmail.com>
2015-11-24 15:27:24 -0600

    Updated to show the use of state_logging method
    When using this, it will create a controller for facereader, start recording, turn on state logging, retrieve the first valid state log(facial emotion) it can find and then stop recording and end the connection to facereader.

--- FaceReader 5.0 API/Implementation.py
+++ FaceReader 5.0 API/Implementation.py
@@ -3,11 +3,12 @@ __author__ = 'Joshua Zosky'
 import FaceReaderBindCPython as FRB
 
 ## Instantiate the controller with IP address(string) and Port number(integer)
-a = FRB.Controller('127.0.0.1', 9090)
+#a = FRB.Controller('127.0.0.1', 9090)
+a = FRB.Controller('192.168.1.1', 9090)
 
 ## Connect to FaceReader
 a.connect()
-
+a.start_recording()
 ## Turn on State Logging(This is a toggle, True = Turn it on, False = Turn it off)
 a.state_logging(True)
 
@@ -15,8 +16,9 @@ a.state_logging(True)
 while True:
     if a.state_log != "":
         print a.state_log
-        False
+        break
 
 ## Disconnect from FaceReader
+a.stop_recording()
 a.disconnect()
 print 'done'
